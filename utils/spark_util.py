085b54d15c0e67c18b26c3f6be334627a9d3b136
bulgakovmyu <50116852+bulgakovmyu@users.noreply.github.com>
2022-04-19 17:19:36 +0300

    first commit

---
+++ utils/spark_util.py
@@ -0,0 +1,44 @@
+import yaml
+from pyspark.sql import SparkSession
+import pyspark
+
+
+def read_config(file):
+    with open(file, "r") as stream:
+        return yaml.safe_load(stream)
+
+
+class AzureConnector(object):
+    def __init__(self, config):
+        self.session = (SparkSession.builder.getOrCreate())
+
+        self.session.conf.set("fs.azure.account.auth.type.bd201stacc.dfs.core.windows.net" ,
+                              config["fs.azure.account.auth.type"])
+        self.session.conf.set(
+                        "fs.azure.account.oauth.provider.type.bd201stacc.dfs.core.windows.net",
+                        config["fs.azure.account.oauth.provider.type"],
+                        )
+        self.session.conf.set(
+                        "fs.azure.account.oauth2.client.id.bd201stacc.dfs.core.windows.net",
+                        config["fs.azure.account.oauth2.client.id"],
+                        )
+        self.session.conf.set(
+                        "fs.azure.account.oauth2.client.secret.bd201stacc.dfs.core.windows.net",
+                        config["fs.azure.account.oauth2.client.secret"],
+                        )
+        self.session.conf.set(
+                        "fs.azure.account.oauth2.client.endpoint.bd201stacc.dfs.core.windows.net",
+                        config["fs.azure.account.oauth2.client.endpoint"],
+                        )
+    def get_session(self):
+        return self.session
+
+
+class AsureReader(object):
+    def __init__(self, session:pyspark.sql.session.SparkSession, data_path:str, file_format:str):
+        self.session = session
+        self.data_path = data_path
+        self.file_format = file_format
+
+    def read(self):
+        return (self.session.read.format(self.file_format).options(header='true', inferschema='true').load(self.data_path))
